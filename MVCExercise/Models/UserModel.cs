877057251c2531334f42813fb9ca5821020c399d
Zsolt Kardos <zsolt.kardos@hotmail.hu>
2021-01-09 17:23:53 +0100

    create solution

---
+++ MVCExercise/Models/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace MVCExercise.Models
+{
+    public class UserModel : IdentityUser
+    {
+        public DateTime Registrated { get; set; }
+        public DateTime LastLogin { get; set; }
+    }
+}
