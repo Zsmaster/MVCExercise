428fcf720dadc2b540e3eb8c68a7ba906a3ca882
Zsolt Kardos <zsolt.kardos@hotmail.hu>
2021-01-09 18:28:44 +0100

    Create database context

---
+++ MVCExercise.Model/Membership/UserModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+
+namespace MVCExercise.Model.Membership
+{
+    public class UserModel : IdentityUser
+    {
+        public DateTime Registrated { get; set; }
+        public DateTime LastLogin { get; set; }
+    }
+}
---
+++ MVCExercise.Repository/MyDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCExercise.Model.Membership;
+
+namespace MVCExercise.Repository
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<UserModel> Users { get; set; }
+    }
+}
